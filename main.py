ec0ebc3b548892afe14a28bf8b538df7fb6e442a
mainor martinez <martimai000@warren.k12.in.us>
2024-11-11 09:52:37 -0500

    initial

---
+++ main.py
@@ -0,0 +1,15 @@
+def calculator(num1, num2, operator):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*" :
+        return num1 * num2
+    elif operator == "/" :
+        return num1 / num2
+
+
+print(calculator(10, 5, '+'))  # Output: 15
+print(calculator(10, 5, '-'))  # Output: 5
+print(calculator(10, 5, '*'))  # Output: 50
+print(calculator(10, 5, '/'))  # Output: 2.0
